531fbe258fc66d5163271e073e2f7f20465fa988
Phillip Jackson <unisys12@gmail.com>
2016-04-27 19:18:20 -0500

    add methods getAllItems w/params and getSingleItem

--- src/Requests/Items.php
+++ src/Requests/Items.php
@@ -4,10 +4,30 @@ namespace palamon\Requests;
 
 class Items {
 
-	public function getItems(){
+	public function getAllItems(){
 
 		$client = new \GuzzleHttp\Client(['headers' => ['X-API-KEY' => $_ENV['BUNGIE_KEY']]]);
 		$req = $client->request('GET', $_ENV['BUNGIE_ITEMS_URL']);
+		$res = json_decode($req->getBody(), true);
+
+		return $res;
+
+	}
+
+	public function getAllItems($parms){
+
+		$client = new \GuzzleHttp\Client(['headers' => ['X-API-KEY' => $_ENV['BUNGIE_KEY']]]);
+		$req = $client->request('GET', $_ENV['BUNGIE_ITEMS_URL'] . '?' . $parms);
+		$res = json_decode($req->getBody(), true);
+
+		return $res;
+
+	}
+
+	public function getSingleItem($key){
+
+		$client = new \GuzzleHttp\Client(['headers' => ['X-API-KEY' => $_ENV['BUNGIE_KEY']]]);
+		$req = $client->request('GET', 'http://www.bungie.net/platform/Destiny/Manifest/InventoryItem/' . $key);
 		$res = json_decode($req->getBody(), true);
 
 		return $res;
