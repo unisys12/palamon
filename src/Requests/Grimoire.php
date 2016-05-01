27c3eb5afe07762361d6c0964c31237191239db6
Phillip Jackson <unisys12@gmail.com>
2016-05-01 10:43:46 -0500

    move from using env vars to const

--- src/Requests/Grimoire.php
+++ src/Requests/Grimoire.php
@@ -4,10 +4,12 @@ namespace palamon\Requests;
 
 class Grimoire {
 
+	const GrimoreURL = "http://www.bungie.net/Platform/Destiny/Vanguard/Grimoire/Definition/"
+
 	public function getGrimoire()
     {
     	$client = new \GuzzleHttp\Client(['headers' => ['X-API-KEY' => $_ENV['BUNGIE_KEY']]]);
-		$req = $client->request('GET', $_ENV['BUNGIE_GRIMOIRE_URL']);
+		$req = $client->request('GET', self::GrimoreURL);
 		$res = json_decode($req->getBody(), true);
 
 		return $res;
