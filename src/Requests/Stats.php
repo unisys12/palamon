ef722aa103472c064bb5e2b5a87c01f31d4519ee
Phillip Jackson <unisys12@gmail.com>
2016-05-01 10:51:36 -0500

    move from using env vars to const

--- src/Requests/Stats.php
+++ src/Requests/Stats.php
@@ -4,10 +4,12 @@ namespace palamon\Requests;
 
 class Stats {
 
+	const StatsURL = "http://www.bungie.net/Platform/Destiny/Stats/Definition/";
+
 	public function getStats(){
 
 		$client = new \GuzzleHttp\Client(['headers' => ['X-API-KEY' => $_ENV['BUNGIE_KEY']]]);
-		$req = $client->request('GET', $_ENV['BUNGIE_STATS_URL']);
+		$req = $client->request('GET', self::StatsURL);
 		$res = json_decode($req->getBody(), true);
 
 		return $res;
