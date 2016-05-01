6e4d982f7ab0a8c8386f0b45bc406d393e88cb9d
Phillip Jackson <unisys12@gmail.com>
2016-05-01 10:32:14 -0500

    add optional param passing to method

--- src/Requests/TalentNodes.php
+++ src/Requests/TalentNodes.php
@@ -4,14 +4,18 @@ namespace palamon\Requests;
 
 class TalentNodes {
 
-	public function getTalentNodes(){
+	const TalentNodes = 'http://www.bungie.net/Platform/Destiny/Explorer/TalentNodeSteps';
+
+	public function getTalentNodes($params){
 
 		$client = new \GuzzleHttp\Client(['headers' => ['X-API-KEY' => $_ENV['BUNGIE_KEY']]]);
-		$req = $client->request('GET', $_ENV['BUNGIE_TALENTNODES_URL']);
+
+		$req = $client->request('GET', isset($params) ? self::TalentNodes . "?" . $params : self::TalentNodes);
+
 		$res = json_decode($req->getBody(), true);
 
 		return $res;
 
-	}
+	}	
 
 }
