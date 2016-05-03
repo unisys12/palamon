12ada0450ad5aec70f56116a5ddf567de31b4f8f
Phillip Jackson <unisys12@gmail.com>
2016-05-03 18:50:23 -0500

    add test for grimoire api response

--- tests/requestTests/grimoireTest.php
+++ tests/requestTests/grimoireTest.php
@@ -6,8 +6,18 @@ class grimoireTestCase extends PHPUnit_Framework_TestCase{
 
 	public function testGrimoireInstance()
 	{
+		//Test if class can be created
 		$grimoire = new Grimoire();
 		$this->assertNotFalse($grimoire);
+	}
+
+	public function testGrimoireResponse()
+	{
+		$grimoire = new Grimoire();
+
+		$request = $grimoire->getGrimoire();
+		$this->assertTrue($request);
+		$this->assertIsArray($request);
 	}
 
 }
