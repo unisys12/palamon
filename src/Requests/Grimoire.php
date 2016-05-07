01fd58bcb14fc0c55568021429c7649779e1700d
Phillip Jackson <unisys12@gmail.com>
2016-05-06 19:04:25 -0500

    update method to insert key in constructor instead of in class

--- src/Requests/Grimoire.php
+++ src/Requests/Grimoire.php
@@ -6,9 +6,14 @@ class Grimoire
 
 	const GRIMOIRE_URL = "http://www.bungie.net/Platform/Destiny/Vanguard/Grimoire/Definition/";
 
+    public function __construct($key)
+    {
+        $this->key = $key;
+    }
+
 	public function getGrimoire()
     {
-    	$client = new \GuzzleHttp\Client(['headers' => ['X-API-KEY' => $_ENV['BUNGIE_KEY']]]);
+    	$client = new \GuzzleHttp\Client(['headers' => ['X-API-KEY' => $this->key]]);
 		$req = $client->request('GET', self::GRIMOIRE_URL);
 		$res = json_decode($req->getBody(), true);
 
