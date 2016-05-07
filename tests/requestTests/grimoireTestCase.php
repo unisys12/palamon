9f125674ec330928ab6ca9bc4006fd6c60f77957
Phillip Jackson <unisys12@gmail.com>
2016-05-06 19:05:53 -0500

    intial test commit (learning)

--- tests/requestTests/grimoireTestCase.php
+++ tests/requestTests/grimoireTestCase.php
@@ -2,10 +2,23 @@
 namespace palamon\tests;
 
 use palamon\Requests\Grimoire;
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Exception\RequestException;
 
 class grimoireTestCase extends \PHPUnit_Framework_TestCase
 {
 
-	
+	public function testGrimoireResponse()
+    {
+        
+        $request = new Grimoire('7ced29b9f06844efb9102fbf73218362');
+        $response = $request->getGrimoire();
+        $this->assertArrayHasKey('Response', $response);
+
+    }
 
 }
