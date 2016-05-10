d674b9be62e1e0561259a8df78129fa65158070f
Phillip Jackson <unisys12@gmail.com>
2016-05-10 07:06:45 -0500

    revert back to original usage

--- src/Requests/Grimoire.php
+++ src/Requests/Grimoire.php
@@ -6,14 +6,9 @@ class Grimoire
 
 	const GRIMOIRE_URL = "http://www.bungie.net/Platform/Destiny/Vanguard/Grimoire/Definition/";
 
-    public function __construct($key)
-    {
-        $this->key = $key;
-    }
-
 	public function getGrimoire()
     {
-    	$client = new \GuzzleHttp\Client(['headers' => ['X-API-KEY' => $this->key]]);
+    	$client = new \GuzzleHttp\Client(['headers' => ['X-API-KEY' => $_ENV['BUNGIE_KEY']]]);
 		$req = $client->request('GET', self::GRIMOIRE_URL);
 		$res = json_decode($req->getBody(), true);
 
